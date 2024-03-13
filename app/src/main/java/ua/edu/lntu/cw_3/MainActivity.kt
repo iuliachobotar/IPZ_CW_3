80b2177f18b209fad0c6dab8450c2399dbdf7f34
iuliachobotar <chobotar_ly@ukr.net>
2024-03-13 15:42:02 +0200

    new

--- app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_3/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -41,35 +37,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WellnessLayout() {
+    LazyColumn(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        item {
+            Text(
+                text = "30 днів рецептів",
+                modifier = Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+        }
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(
-            text = "30 днів рецептів",
-            modifier = Modifier.fillMaxWidth(),
-            style = MaterialTheme.typography.titleLarge
-        )
-        Spacer(modifier = Modifier.height(30.dp))
-        DayLayout(
-            dayNumber = 1,
-            title = "Романтична яєчня",
-            imagePainter = painterResource(id = R.drawable.photo1),
-            description = "Сосиски для рецепту вибирайте довгі. Розріжте кожну сосиску вздовж, але не до кінця. Залиште без розрізу приблизно 1 см. З двох половинок сосиски сформуйте серце і закріпіть знизу зубочисткою.\n" +
-                    "\n" +
-                    "Обсмажте сосиски-серця на розігрітій з олією сковороді з одного боку. Потім переверніть на інший бік і вбийте усередину кожної сосиски по одному яйцю. Зменшіть вогонь та готуйте страву під закритою кришкою 7-8 хвилин. При подачі витягніть зубочистку і за бажанням прикрасьте яєчню кетчупом або майонезом."
-        )
+        item {
+            DayLayout(
+                dayNumber = 1,
+                title = "Романтична яєчня",
+                imagePainter = painterResource(id = R.drawable.photo1),
+                description = "Сосиски для рецепту вибирайте довгі. Розріжте кожну сосиску вздовж, але не до кінця. Залиште без розрізу приблизно 1 см. З двох половинок сосиски сформуйте серце і закріпіть знизу зубочисткою.\n" +
+                        "\n" +
+                        "Обсмажте сосиски-серця на розігрітій з олією сковороді з одного боку. Потім переверніть на інший бік і вбийте усередину кожної сосиски по одному яйцю. Зменшіть вогонь та готуйте страву під закритою кришкою 7-8 хвилин. При подачі витягніть зубочистку і за бажанням прикрасьте яєчню кетчупом або майонезом."
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+        }
 
-        Spacer(modifier = Modifier.height(30.dp))
-
-        DayLayout(
-            dayNumber = 2,
-            title = "М'ясо по-королівськи",
-            imagePainter = painterResource(id = R.drawable.photo2),
-            description = "Наріжте порціями свинячу вирізку, відбийте, посоліть і поперчіть. Залиште на 30 хвилин маринуватися. Цибулю та помідори наріжте півкільцями, картоплю - тонкими часточками, а печериці - пластинками.\n" +
-                    "\n" +
-                    "У глибоку форму для запікання влийте олію та викладіть шматочки м'яса. Змастіть майонезом і покладіть цибулю. Посоліть і поперчіть. Потім викладіть картоплю, спеції, майонез, гриби, майонез, помідор і знову майонез. Посипте запіканку тертим сиром і запейкайте близько 1 години в духовці при 180 ."
-        )
-
-
+        item {
+            DayLayout(
+                dayNumber = 2,
+                title = "М'ясо по-королівськи",
+                imagePainter = painterResource(id = R.drawable.photo2),
+                description = "Наріжте порціями свинячу вирізку, відбийте, посоліть і поперчіть. Залиште на 30 хвилин маринуватися. Цибулю та помідори наріжте півкільцями, картоплю - тонкими часточками, а печериці - пластинками.\n" +
+                        "\n" +
+                        "У глибоку форму для запікання влийте олію та викладіть шматочки м'яса. Змастіть майонезом і покладіть цибулю. Посоліть і поперчіть. Потім викладіть картоплю, спеції, майонез, гриби, майонез, помідор і знову майонез. Посипте запіканку тертим сиром і запейкайте близько 1 години в духовці при 180 ."
+            )
+        }
     }
 }
 
